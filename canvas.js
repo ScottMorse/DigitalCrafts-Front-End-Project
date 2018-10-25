5c5f3da0e753f972abf0e04c23d8831815f7d635
Christina Truong <christina.truong0@gmail.com>
2018-10-25 09:17:52 -0500

    wildcard pop up working

--- canvas.js
+++ canvas.js
@@ -1,4 +1,13 @@
-
+let wildcardOptions = ["Possible results for a 'wildcard' slice on the spinning wheel.","Complete a jig-saw puzzle","Pet a dog","Pet a cat",
+"Go for a walk with a dog","Go for a walk with a cat","Play rock paper scissors","Purchase a rubber chicken and bother your neighbor with it",
+"Frame your face and print it onto a T-shirt","Frame your dog's face and print it onto a T-shirt","Frame your cat's face and print it onto a T-shirt",
+"Fill water balloons and have a battle","Change Siri or Alexa to 2x talking speed and ask her 5 questions",
+"Start a to-do list of all the things you've been procrastinating","Treat yourself to a delicious bowl of your favorite ice cream",
+"Go to the grocery store and race your friend with shopping karts","Listen to your favorite song of the early 2000's",
+"Listen to your favorite boy-band song of the early 2000's","Listen to your favorite Backstreet Boys song from the early 2000's",
+"Find loose change from around your house and car and visit the gaming arcade","Play catch with a football/baseball/tennis ball. Preferably all three at once.",
+"Dust off your favorite book and relax on your favorite piece of furniture","Meditate for 5 minutes and clear your mind of all distractions.",
+"Stand up and stretch out your stiff muscles. Take deep breaths."]
 
 
 var options = ["Cook dinner", "Try again", "Eat out", "Wildcard", "Movies","Cook dinner", "Try again", "Eat out", "Wildcard", "Movies"];
@@ -109,7 +118,7 @@ function responsiveWheel() {
 
     var wheelRadius = wheelSize/2
 
-    var outsideRadius = wheelRadius - 5;
+    var outsideRadius = wheelRadius - 40;
     var textRadius = wheelSize/3;
     var insideRadius = wheelRadius / 5;
 
@@ -221,6 +230,7 @@ function stopRotateWheel() {
   const moviePop = document.getElementById('movie-pop')
   const restaurantPop = document.getElementById('restaurant-pop')
   const recipePop = document.getElementById('recipe-pop')
+  const wildPop = document.getElementById('wild-pop')
   function displayPopup(selectedtoption){
   if (selectedtoption=="Movies"){
       showPopUp(moviePop)
@@ -231,6 +241,12 @@ function stopRotateWheel() {
    if (selectedtoption=="Cook dinner"){
     showPopUp(recipePop)
   }
+  if (selectedtoption=="Wildcard"){
+      let randomCardOption = Math.floor(Math.random() * wildcardOptions.length)
+      let wildCardRand = wildcardOptions[randomCardOption]
+      document.getElementById('wild-text').innerHTML=wildCardRand
+      showPopUp(wildPop)
+    }
   }
 
 function easeOut(t, b, c, d) {
