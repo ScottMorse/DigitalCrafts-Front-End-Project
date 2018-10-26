6e95784a6fea39c5baf6b260c63aec81247a6e56
Anthony Trevino <anthonytrevinolol@yahoo.com>
2018-10-26 11:28:34 -0500

    updated wildcard items

--- wildcard/wildcard.js
+++ wildcard/wildcard.js
@@ -1,7 +1,8 @@
 // if spinner lands in wildcard area
 
 
-wildcardOptions = ["Possible results for a 'wildcard' slice on the spinning wheel.","Complete a jig-saw puzzle","Pet a dog","Pet a cat","Go for a walk with a dog","Go for a walk with a cat","Play rock paper scissors","Purchase a rubber chicken and bother your neighbor with it","Frame your face and print it onto a T-shirt","Frame your dog's face and print it onto a T-shirt","Frame your cat's face and print it onto a T-shirt","Fill water balloons and have a battle","Change Siri or Alexa to 2x talking speed and ask her 5 questions","Start a to-do list of all the things you've been procrastinating","Treat yourself to a delicious bowl of your favorite ice cream","Go to the grocery store and race your friend with shopping karts","Listen to your favorite song of the early 2000's","Listen to your favorite boy-band song of the early 2000's","Listen to your favorite Backstreet Boys song from the early 2000's","Find loose change from around your house and car and visit the gaming arcade","Play catch with a football/baseball/tennis ball. Preferably all three at once.","Dust off your favorite book and relax on your favorite piece of furniture","Meditate for 5 minutes and clear your mind of all distractions.","Stand up and stretch out your stiff muscles. Take deep breaths."]
+
+wildcardOptions = ["img src='yellowchicken.gif'","Pet a dog","Pet a cat","Go for a walk with a dog","Go for a walk with a cat","Play rock paper scissors","img src=\"chickencar.gif\"","Frame your face and print it onto a T-shirt","Frame your dog's face and print it onto a T-shirt","Frame your cat's face and print it onto a T-shirt","Fill water balloons and have a battle","Change Siri or Alexa to 2x talking speed and ask her 5 questions","Start a to-do list of all the things you've been procrastinating","Go to the grocery store and race your friend with shopping karts","Listen to your favorite song of the early 2000's","'yellowchicken.gif'","Listen to your favorite Backstreet Boys song from the early 2000's","Find loose change from around your house and car and visit the gaming arcade","Play catch with a football/baseball/tennis ball. Preferably all three at once.","Dust off your favorite book and relax on your favorite piece of furniture","Meditate for 5 minutes and clear your mind of all distractions."]
 
 
 let randomOption = Math.floor(Math.random() * wildcardOptions.length)
@@ -10,5 +11,6 @@ if(wildcard = true) {
 for(var i = 0; i < wildcardOptions.length; i++) {
 
 
+
+  }
 }console.log(wildcardOptions[randomOption])
-}
