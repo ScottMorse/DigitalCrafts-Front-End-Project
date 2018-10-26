65c2f0831ea972746ca121e27f3bbb99b903c291
Scott Morse <scottmorsedev@gmail.com>
2018-10-26 13:30:53 -0500

    Fixed some style based on suggestions.

--- loadin.js
+++ loadin.js
@@ -2,11 +2,11 @@ const loader = document.getElementById('loader')
 const loadMask = document.getElementById('load-mask')
 
 function runIntro(){
-    setTimeout(()=>loadMask.style.transform = 'translateX(100vw)',100)
+    setTimeout(()=>loadMask.style.transform = 'translateX(3000px)',100)
     setTimeout(()=>{
         loader.style.opacity = 0
-        setTimeout(()=>loader.style.display="none",1000)
-    },1000)
+        setTimeout(()=>loader.style.display="none",1400)
+    },1400)
 }
 
 window.addEventListener('load',runIntro)
