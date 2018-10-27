559084dc4fe57ece22b3c80a3b50880999758d87
Scott Morse <scottmorsedev@gmail.com>
2018-10-26 20:15:29 -0500

    Fixed the favorites menu overlapping.

--- menu.js
+++ menu.js
@@ -1,5 +1,8 @@
+const allSubMenus = Array.from(document.querySelectorAll('.dropdown-menu .dropdown-submenu'))
+
 $(document).ready(function(){
   $('.dropdown-submenu a.test').on("click", function(e){
+    $('.dropdown-submenu .dropdown-menu').each((index,ul) => $(ul).hide())
       $(this).next('ul').toggle();
       e.stopPropagation();
       e.preventDefault();
